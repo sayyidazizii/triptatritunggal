99487b4e1a3e38941868fd7fde0168df6c651130
Vinsmoke-Tech <daffahanaris@gmail.com>
2024-12-19 17:03:08 +0700

    update (Core Customer) = change flow Core Customer Controller

--- app/Http/Controllers/CoreCustomerController.php
+++ app/Http/Controllers/CoreCustomerController.php
@@ -2,158 +2,228 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use App\Models\CoreCustomer;
-use App\Models\CoreProvince;
-use App\Models\CoreCity;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use App\Models\AcctDebtRepayment;
+use App\Models\AcctDebtRepaymentItem;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB; 
+use Carbon\Carbon;
 
 class CoreCustomerController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        $corecustomer = CoreCustomer::where('data_state','=',0)->get();
-
-        return view('content/CoreCustomer/ListCoreCustomer',compact('corecustomer'));
+        Session::forget('datacustomer');
+        $data = CoreCustomer::select('*')
+        ->where('data_state',0)
+        ->get();
+        return view('content.CoreCustomer.ListCoreCustomer', compact('data'));
     }
 
-    public function addCoreCustomer(Request $request)
+    public function addElementsCoreCustomer(Request $request)
     {
-        $province = CoreProvince::where('data_state',0)
-        ->pluck('province_name', 'province_id');
+        $datacustomer = Session::get('datacustomer');
+        if(!$datacustomer || $datacustomer == ''){
+            $datacustomer['customer_number']    = '';
+            $datacustomer['customer_name']      = '';   
+            $datacustomer['debt_limit']         = '';   
+        }
+        $datacustomer[$request->name] = $request->value;
+        Session::put('datacustomer', $datacustomer);
+    }
 
-        return view('content/CoreCustomer/FormAddCoreCustomer', compact('province'));
+    public function resetElementsCoreCustomer()
+    {
+        Session::forget('datacustomer');
+
+        return redirect()->back();
+    }
+
+    public function addCoreCustomer()
+    {
+        $customer = Session::get('datacustomer');
+
+        return view('content.CoreCustomer.AddCoreCustomer', compact('customer'));
     }
 
     public function processAddCoreCustomer(Request $request)
     {
-        $fields = $request->validate([
-            'customer_name' => 'required',
-            'customer_code' => 'required',
-            'province_id'   => 'required',
-            'city_id'       => 'required',
-        ]);
-        
+        $request->validate(['customer_name' => 'required']);
 
-        $item = CoreCustomer::create([
-            'customer_name'                 => $fields['customer_name'], 
-            'customer_code'                 => $fields['customer_code'], 
-            'province_id'                   => $fields['province_id'],   
-            'city_id'                       => $fields['city_id'],
-            'customer_address'              => $request->customer_address,
-            'customer_home_phone'           => $request->customer_home_phone,
-            'customer_mobile_phone1'        => $request->customer_mobile_phone1,
-            'customer_mobile_phone2'        => $request->customer_mobile_phone2,
-            'customer_fax_number'           => $request->customer_fax_number,
-            'customer_email'                => $request->customer_email,
-            'customer_contact_person'       => $request->customer_contact_person,
-            'customer_tax_no'               => $request->customer_tax_no,
-            'customer_payment_terms'        => $request->customer_payment_terms,
-            'customer_remark'               => $request->customer_remark,
-            'created_id'                    => Auth::id(),
-            'data_state'                    => 0
-        ]);
+        try {
+            DB::beginTransaction();
+            
+            $data = $request->all();
+            
+                CoreCustomer::create($data);
 
-        $msg = 'Tambah customer Berhasil';
-        return redirect('/customer/add')->with('msg',$msg);
-    }
+                $msg = 'Tambah Pegawai Berhasil';
 
-    public function editCoreCustomer($customer_id)
-    {
-        $province = CoreProvince::where('data_state',0)
-        ->pluck('province_name', 'province_id');
+            DB::commit();
 
-        $customer = CoreCustomer::where('customer_id',$customer_id)->first();
+                return redirect()->back()->with('msg', $msg);
 
-        $city     = CoreCity::where('data_state',0)
-        ->where('province_id', $customer['province_id'])
-        ->pluck('city_name', 'city_id');
-
-        return view('content/CoreCustomer/FormEditCoreCustomer',compact('customer', 'province', 'city'));
-    }
-
-    public function processEditCoreCustomer(Request $request)
-    {
-        $fields = $request->validate([
-            'customer_id'   => 'required',
-            'customer_name' => 'required',
-            'customer_code' => 'required',
-            'province_id'   => 'required',
-            'city_id'       => 'required',
-        ]);
-
-        $item = CoreCustomer::findOrFail($fields['customer_id']);
-        $item->customer_name                    = $fields['customer_name'];
-        $item->customer_code                    = $fields['customer_code'];
-        $item->province_id                      = $fields['province_id'];
-        $item->city_id                          = $fields['city_id'];
-        $item->customer_address                 = $request->customer_address;
-        $item->customer_home_phone              = $request->customer_home_phone;
-        $item->customer_mobile_phone1           = $request->customer_mobile_phone1;
-        $item->customer_mobile_phone2           = $request->customer_mobile_phone2;
-        $item->customer_fax_number              = $request->customer_fax_number;
-        $item->customer_email                   = $request->customer_email;
-        $item->customer_contact_person          = $request->customer_contact_person;
-        $item->customer_tax_no                  = $request->customer_tax_no;
-        $item->customer_payment_terms           = $request->customer_payment_terms;
-        $item->customer_remark                  = $request->customer_remark;
-
-        if($item->save()){
-            $msg = 'Edit customer Berhasil';
-            return redirect('/customer')->with('msg',$msg);
-        }else{
-            $msg = 'Edit customer Gagal';
-            return redirect('/customer')->with('msg',$msg);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            report($e);
+            
+            $msg = 'Tambah Pegawai Gagal';
+            return redirect()->back()->with('msg', $msg);
         }
     }
 
-    public function deleteCoreCustomer($item_unit_id)
+     public function editCoreCustomer($customer_id)
+     {
+         $data = CoreCustomer::select('customer_number','customer_name','debt_limit','customer_id')
+         ->where('customer_id', $customer_id)
+         ->first();
+
+         return view('content.CoreCustomer.EditCoreCustomer', compact('data'));
+     }
+
+     public function processEditCoreCustomer(Request $request)
+     {
+         $table                     = CoreCustomer::findOrFail($request->customer_id);
+         $table->customer_number    = $request->customer_number;
+         $table->customer_name      = $request->customer_name;
+         $table->debt_limit         = $request->debt_limit;
+         $table->updated_id         = Auth::id();
+
+        //  echo json_encode( $table); exit;
+         
+         if ($table->save()) {
+             $msg = 'Ubah Pegawai Berhasil';
+             return redirect()->back()->with('msg', $msg);
+         } else {
+             $msg = 'Ubah Pegawai Gagal';
+             return redirect()->back()->with('msg', $msg);
+         }
+
+
+     }
+
+     public function processEditLimitCoreCustomer(Request $request)
     {
-        $item = CoreCustomer::findOrFail($item_unit_id);
-        $item->data_state = 1;
-        if($item->save())
-        {
-            $msg = 'Hapus customer Berhasil';
-        }else{
-            $msg = 'Hapus customer Gagal';
-        }
-
-        return redirect('/customer')->with('msg',$msg);
-    }
-
-    public function getCoreCity(Request $request){
-        $province_id = $request->province_id;
-        $data='';
-
-        $city = CoreCity::where('province_id', $province_id)
-        ->where('data_state','=',0)
+        // print_r($request); exit;
+        $customer = CoreCustomer::select('debt_limit','customer_id','amount_debt','remaining_limit')
+        ->where('data_state',0)
         ->get();
 
-        $data .= "<option value=''>--Choose One--</option>";
-        foreach ($city as $mp){
-            $data .= "<option value='$mp[city_id]'>$mp[city_name]</option>\n";	
-        }
-
-        return $data;
+            foreach ($customer as $p) {
+                $p->remaining_limit     = $request->debt_limit - $p['amount_debt'] ;  
+                $p->debt_limit          = $request->debt_limit;  
+                $p->updated_id          = Auth::id();    
+                $p->save();
+             }
+                
+            if ($p->save()) {
+                $msg = 'Ubah Limit Berhasil';
+                return redirect()->back()->with('msg', $msg);
+                } else {
+                        $msg = 'Ubah Limit Gagal';
+                        return redirect()->back()->with('msg', $msg);
+                }
     }
-}
+
+    public function editlimitCoreCustomer()
+     {
+         $customer = CoreCustomer::select('customer_number','customer_name','debt_limit','customer_id')
+         ->where('data_state',0)
+         ->get();
+        
+         return view('content.CoreCustomer.EditLimitCoreCustomer', compact('customer'));
+     }
+
+    public function deleteCoreCustomer($customer_id)
+    {
+        $table              = CoreCustomer::findOrFail($customer_id);
+        $table->data_state  = 1;
+        $table->updated_id  = Auth::id();
+
+        if ($table->save()) {
+            $msg = 'Hapus Pegawai Berhasil';
+            return redirect()->back()->with('msg', $msg);
+        } else {
+            $msg = 'Hapus Pegawai Gagal';
+            return redirect()->back()->with('msg', $msg);
+        }
+    }
+
+    public function deletedebtCoreCustomer(Request $request)
+    {
+        // mentotal jumlah hutang pegaawai 
+        $customer = CoreCustomer::select('amount_debt','customer_id')
+        ->where('data_state',0)
+        ->get();
+
+            //looping
+            foreach($customer as $p){
+                $amount_debt = $request['total_repayment'] += $p['amount_debt'] ;
+                $p->amount_debt = $amount_debt;
+            }
+
+          $debtrepayment = array(
+              'total_repayment'                 => $request->total_repayment,
+              'debt_repayment_date'             => Carbon::now(),
+              'updated_id'                      => Auth::id(),
+              'created_id'                      => Auth::id(),
+          );
+             
+        if(AcctDebtRepayment::create($debtrepayment)){
+                $lastdebtrepayment = AcctDebtRepayment::where('created_id',Auth::id())
+                ->orderBy('created_at', 'DESC')
+                ->first();
+
+                $customer = CoreCustomer::select('amount_debt','customer_id')
+                ->where('data_state',0)
+                ->get();
+         
+                foreach($customer as $p){
+                    $amount_debt    = $request['debt_repayment_amount'] = $p['amount_debt'];
+                    $customer_id    = $request['customer_id'] = $p['customer_id'];
+                    $p->amount_debt = $amount_debt;
+                    $p->customer_id = $customer_id;
+             
+                    $debtrepayment = array(
+                        'debt_repayment_id'     => $lastdebtrepayment['debt_repayment_id'],
+                        'customer_id'           =>  $request->customer_id,
+                        'debt_repayment_amount' => $request->debt_repayment_amount,
+                        'updated_id'            => Auth::id(),
+                        'created_id'            => Auth::id(),    
+                    );
+                    AcctDebtRepaymentItem::create($debtrepayment);
+                }
+        }
+                // echo json_encode($debtrepayment); exit;
+                
+
+            // hapus pelunasan hutang customer
+                $customer = CoreCustomer::select('amount_debt','remaining_limit','customer_id')
+                ->where('data_state',0)
+                ->get();
+                
+                foreach ($customer as $p) {
+                    $p->remaining_limit = $p['amount_debt'] + $p['remaining_limit'];
+                    $p->amount_debt     = 0;
+                    $p->updated_id      = Auth::id();
+                    $p->save();
+                }
+                
+            if ($p->save()) {
+             $msg = 'Pelunasan Hutang Berhasil';
+             return redirect()->back()->with('msg', $msg);
+            } else {
+                $msg = 'Pelunasan Hutang Gagal';
+                return redirect()->back()->with('msg', $msg);
+            }            
+         }
+    }
+
