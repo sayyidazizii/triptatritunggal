1c8342cd29438233641b7dfa26ce5c8b58d4c5de
Vinsmoke-Tech <daffahanaris@gmail.com>
2025-02-04 11:44:47 +0700

    update(ModalCategory)

--- resources/views/content/SalesQuotation/Modal/ModalAddCategory.blade.php
+++ resources/views/content/SalesQuotation/Modal/ModalAddCategory.blade.php
@@ -1,27 +1,22 @@
 {{-- Modal Tambahan untuk menambahkan data barang --}}
-<div class="modal fade bs-modal-lg" id="addKetegori" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal fade bs-modal-lg" id="addcategory" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header"  style='text-align:left !important'>
-                <h4>Form Tambah Kategori</h4>
+                <h4>Form Tambah Kategori Barang</h4>
             </div>
             <div class="modal-body">
-            <div class="row form-group">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <a class="text-dark">Nama Barang</a>
-                            <input class="form-control input-bb" type="text" name="item_type_name" id="item_type_name"/>
-                        </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <a class="text-dark">Kategori Barang</a>
+                        <input class="form-control input-bb" type="text" name="item_category_name" id="item_category_name" value=""/>
                     </div>
+                </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal" id='cancel_btn_category'>Batal</button>
-                {{-- <a class="btn btn-primary" onClick="addInvType()">Simpan</a> --}}
-                <a type="submit" name="Save" class="btn btn-primary" id="btn_save_category" title="Save" onclick="addCategory()">
-                        <i class="fa fa-plus"></i> Tambah
-                </a>
+                <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal" id='cancel-btn-category'>Batal</button>
+                <a class="btn btn-primary btn-sm" onClick="addCategory()">Simpan</a>
             </div>
-        </div>
         </div>
     </div>
 </div>
