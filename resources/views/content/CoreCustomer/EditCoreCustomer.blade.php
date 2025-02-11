9afdc3a974f19f6b441872796f32a36d85e0eb57
Vinsmoke-Tech <daffahanaris@gmail.com>
2025-02-11 15:45:25 +0700

    update()

--- resources/views/content/CoreCustomer/EditCoreCustomer.blade.php
+++ resources/views/content/CoreCustomer/EditCoreCustomer.blade.php
@@ -29,7 +29,7 @@
 @if(count($errors) > 0)
 <div class="alert alert-danger" role="alert">
     @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
+            <li>{{ $error }}</li>
     @endforeach
 @endif
 </div>
